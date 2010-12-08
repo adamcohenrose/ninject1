99613f6bf2692403612a76f615a440c54b28db46
Adam Cohen-Rose <github.com@cohen-rose.org>
2010-12-08 12:08:53 +0000

    refixed issue with .NET 2.0 SP1 as per http://groups.google.com/group/ninject/browse_thread/thread/6961f74cf141a9ca/79d7ea42a8c1d93c?pli=1 -- Adam Cohen-Rose

--- src/Core/Parameters/Collection/ParameterCollection.cs
+++ src/Core/Parameters/Collection/ParameterCollection.cs
@@ -18,6 +18,7 @@
 #endregion
 #region Using Directives
 using System;
+using System.Collections.Generic;
 using Ninject.Core.Activation;
 using Ninject.Core.Infrastructure;
 #endregion
@@ -28,8 +29,169 @@ namespace Ninject.Core.Parameters
 	/// A collection that organizes parameters by type.
 	/// </summary>
 	public class ParameterCollection : TypedCollection<string, IParameter>, IParameterCollection
-	{
-		/*----------------------------------------------------------------------------------------*/
+    {
+        /*----------------------------------------------------------------------------------------
+         * See http://groups.google.com/group/ninject/browse_thread/thread/6961f74cf141a9ca/79d7ea42a8c1d93c#79d7ea42a8c1d93c
+         *----------------------------------------------------------------------------------------*/
+        #region Interface implementation
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Adds the specified item to the collection.
+        /// </summary>
+        /// <typeparam name="T">The type to organize the item under.</typeparam>
+        /// <param name="item">The item to add.</param>
+        void ITypedCollection<string, IParameter>.Add<T>(T item)
+        {
+            base.Add<T>(item);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Adds the specified item to the collection.
+        /// </summary>
+        /// <param name="type">The type to organize the item under.</param>
+        /// <param name="item">The item to add.</param>
+        void ITypedCollection<string, IParameter>.Add(Type type, IParameter item)
+        {
+            base.Add(type, item);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Adds the specified items to the collection.
+        /// </summary>
+        /// <typeparam name="T">The type to organize the items under.</typeparam>
+        /// <param name="items">The items to add.</param>
+        void ITypedCollection<string, IParameter>.AddRange<T>(IEnumerable<T> items)
+        {
+            base.AddRange<T>(items);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Adds the specified items to the collection.
+        /// </summary>
+        /// <param name="type">The type to organize the items under.</param>
+        /// <param name="items">The items to add.</param>
+        void ITypedCollection<string, IParameter>.AddRange(Type type, IEnumerable<IParameter> items)
+        {
+            base.AddRange(type, items);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets a value indicating whether an item with the specified key has been organized under
+        /// the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type the item is organized under.</typeparam>
+        /// <param name="key">The item's key.</param>
+        /// <returns><see langword="True"/> if the item has been defined, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<string, IParameter>.Has<T>(string key)
+        {
+            return base.Has<T>(key);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets a value indicating whether an item with the specified key has been organized under
+        /// the specified type.
+        /// </summary>
+        /// <param name="type">The type the item is organized under.</param>
+        /// <param name="key">The item's key.</param>
+        /// <returns><see langword="True"/> if the item has been defined, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<string, IParameter>.Has(Type type, string key)
+        {
+            return base.Has(type, key);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets a value indicating whether one or more items organized under the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <returns><see langword="True"/> if there are such items, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<string, IParameter>.HasOneOrMore<T>()
+        {
+            return base.HasOneOrMore<T>();
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets a value indicating whether one or more items organized under the specified type.
+        /// </summary>
+        /// <param name="type">The type check.</param>
+        /// <returns><see langword="True"/> if there are such items, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<string, IParameter>.HasOneOrMore(Type type)
+        {
+            return base.HasOneOrMore(type);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets the item with the specified key, organized under the specified type, if one has been defined.
+        /// </summary>
+        /// <typeparam name="T">The type the item is organized under.</typeparam>
+        /// <param name="key">The item's key.</param>
+        /// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        T ITypedCollection<string, IParameter>.Get<T>(string key)
+        {
+            return base.Get<T>(key);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets the item with the specified key, organized under the specified type, if one has been defined.
+        /// </summary>
+        /// <param name="type">The type the item is organized under.</param>
+        /// <param name="key">The item's key.</param>
+        /// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        IParameter ITypedCollection<string, IParameter>.Get(Type type, string key)
+        {
+            return base.Get(type, key);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets the first item in the collection that is organized under the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        T ITypedCollection<string, IParameter>.GetOne<T>()
+        {
+            return base.GetOne<T>();
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets the first item in the collection that is organized under the specified type.
+        /// </summary>
+        /// <param name="type">The type the item is organized under.</param>
+        /// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        IParameter ITypedCollection<string, IParameter>.GetOne(Type type)
+        {
+            return base.GetOne(type);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets all items organized under the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type the items are organized under.</typeparam>
+        /// <returns>A collection of items organized under the specified type.</returns>
+        IList<T> ITypedCollection<string, IParameter>.GetAll<T>()
+        {
+            return base.GetAll<T>();
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets all items organized under the specified type.
+        /// </summary>
+        /// <param name="type">The type the items are organized under.</param>
+        /// <returns>A collection of items organized under the specified type.</returns>
+        IList<IParameter> ITypedCollection<string, IParameter>.GetAll(Type type)
+        {
+            return base.GetAll(type);
+        }
+        /*----------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Gets the types that items are organized under.
+        /// </summary>
+        /// <returns>A collection of types that items are organized under.</returns>
+        IList<Type> ITypedCollection<string, IParameter>.GetTypes()
+        {
+            return base.GetTypes();
+        }
+        /*----------------------------------------------------------------------------------------*/
+        #endregion
+        /*----------------------------------------------------------------------------------------*/
 		#region Public Methods
 		/// <summary>
 		/// Copies the parameters from the specified collection.
--- src/Core/Planning/Directives/Collection/DirectiveCollection.cs
+++ src/Core/Planning/Directives/Collection/DirectiveCollection.cs
@@ -18,6 +18,7 @@
 #endregion
 #region Using Directives
 using System;
+using System.Collections.Generic;
 using Ninject.Core.Infrastructure;
 using Ninject.Core.Planning.Directives;
 #endregion
@@ -28,8 +29,169 @@ namespace Ninject.Core.Planning
 	/// A collection of binding directives, stored in an activation plan.
 	/// </summary>
 	public class DirectiveCollection : TypedCollection<object, IDirective>, IDirectiveCollection
-	{
+    {
+        /*----------------------------------------------------------------------------------------
+         * See http://groups.google.com/group/ninject/browse_thread/thread/6961f74cf141a9ca/79d7ea42a8c1d93c#79d7ea42a8c1d93c
+         *----------------------------------------------------------------------------------------*/
+        #region Interface implementation
 		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Adds the specified item to the collection.
+		/// </summary>
+		/// <typeparam name="T">The type to organize the item under.</typeparam>
+		/// <param name="item">The item to add.</param>
+        void ITypedCollection<object, IDirective>.Add<T>(T item)
+        {
+            base.Add<T>(item);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Adds the specified item to the collection.
+		/// </summary>
+		/// <param name="type">The type to organize the item under.</param>
+		/// <param name="item">The item to add.</param>
+        void ITypedCollection<object, IDirective>.Add(Type type, IDirective item)
+        {
+            base.Add(type, item);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Adds the specified items to the collection.
+		/// </summary>
+		/// <typeparam name="T">The type to organize the items under.</typeparam>
+		/// <param name="items">The items to add.</param>
+        void ITypedCollection<object, IDirective>.AddRange<T>(IEnumerable<T> items)
+        {
+            base.AddRange<T>(items);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Adds the specified items to the collection.
+		/// </summary>
+		/// <param name="type">The type to organize the items under.</param>
+		/// <param name="items">The items to add.</param>
+        void ITypedCollection<object, IDirective>.AddRange(Type type, IEnumerable<IDirective> items)
+        {
+            base.AddRange(type, items);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets a value indicating whether an item with the specified key has been organized under
+		/// the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type the item is organized under.</typeparam>
+		/// <param name="key">The item's key.</param>
+		/// <returns><see langword="True"/> if the item has been defined, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<object, IDirective>.Has<T>(object key)
+        {
+            return base.Has<T>(key);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets a value indicating whether an item with the specified key has been organized under
+		/// the specified type.
+		/// </summary>
+		/// <param name="type">The type the item is organized under.</param>
+		/// <param name="key">The item's key.</param>
+		/// <returns><see langword="True"/> if the item has been defined, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<object, IDirective>.Has(Type type, object key)
+        {
+            return base.Has(type, key);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets a value indicating whether one or more items organized under the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type to check.</typeparam>
+		/// <returns><see langword="True"/> if there are such items, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<object, IDirective>.HasOneOrMore<T>()
+        {
+            return base.HasOneOrMore<T>();
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets a value indicating whether one or more items organized under the specified type.
+		/// </summary>
+		/// <param name="type">The type check.</param>
+		/// <returns><see langword="True"/> if there are such items, otherwise <see langword="false"/>.</returns>
+        bool ITypedCollection<object, IDirective>.HasOneOrMore(Type type)
+        {
+            return base.HasOneOrMore(type);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets the item with the specified key, organized under the specified type, if one has been defined.
+		/// </summary>
+		/// <typeparam name="T">The type the item is organized under.</typeparam>
+		/// <param name="key">The item's key.</param>
+		/// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        T ITypedCollection<object, IDirective>.Get<T>(object key)
+        {
+            return base.Get<T>(key);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets the item with the specified key, organized under the specified type, if one has been defined.
+		/// </summary>
+		/// <param name="type">The type the item is organized under.</param>
+		/// <param name="key">The item's key.</param>
+		/// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        IDirective ITypedCollection<object, IDirective>.Get(Type type, object key)
+        {
+            return base.Get(type, key);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets the first item in the collection that is organized under the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type to check.</typeparam>
+		/// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        T ITypedCollection<object, IDirective>.GetOne<T>()
+        {
+            return base.GetOne<T>();
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets the first item in the collection that is organized under the specified type.
+		/// </summary>
+		/// <param name="type">The type the item is organized under.</param>
+		/// <returns>The item, or <see langword="null"/> if none has been defined.</returns>
+        IDirective ITypedCollection<object, IDirective>.GetOne(Type type)
+        {
+            return base.GetOne(type);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets all items organized under the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type the items are organized under.</typeparam>
+		/// <returns>A collection of items organized under the specified type.</returns>
+        IList<T> ITypedCollection<object, IDirective>.GetAll<T>()
+        {
+            return base.GetAll<T>();
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets all items organized under the specified type.
+		/// </summary>
+		/// <param name="type">The type the items are organized under.</param>
+		/// <returns>A collection of items organized under the specified type.</returns>
+        IList<IDirective> ITypedCollection<object, IDirective>.GetAll(Type type)
+        {
+            return base.GetAll(type);
+        }
+		/*----------------------------------------------------------------------------------------*/
+		/// <summary>
+		/// Gets the types that items are organized under.
+		/// </summary>
+		/// <returns>A collection of types that items are organized under.</returns>
+        IList<Type> ITypedCollection<object, IDirective>.GetTypes()
+        {
+            return base.GetTypes();
+        }
+		/*----------------------------------------------------------------------------------------*/
+        #endregion
+        /*----------------------------------------------------------------------------------------*/
 		#region Public Methods
 		/// <summary>
 		/// Copies the directives from the specified collection.
